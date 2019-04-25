2b050761a1210c28e4a3089bae37a8a5c78064d1
Chaudhary <K28172@corp.root.nasd.com>
2019-04-25 16:42:13 -0400

    Comment added in InterThread

--- src/com/multithreading/interThread/Inter_Thread.java
+++ src/com/multithreading/interThread/Inter_Thread.java
@@ -5,22 +5,29 @@ class Q{
 	boolean valueSet=false;
 	
 	public synchronized void put(int value) {
-		while(valueSet) {
-			try{wait();} catch(Exception e) {}
+		while(valueSet) {						//When valueSet=true,value is set already, it waits and lets get() get value first
+			try{wait();} catch(Exception e) {}	//then when get() notifies it, it increments and set another value.....
 		}
 		System.out.println("Put: "+value);
 		this.value=value;
 		valueSet=true;
-		notify();
+		notify();			//notifies the get() that it has already been set,and that it can proceed now
 	}
 	
-	public synchronized void get() {
-		while(!valueSet) {
+	/* In 1st iteration, -> If Consumer thread reaches first, valueSet=false so it will enter the while loop and wait
+	 * until the put() method reaches and sets the value. Once the value is set, Put() will notify get() to proceed
+	 * and the valueSet=true now, so get() will exit the while loop and print Get value.
+	 * -> If producer has already reached before Consumer,the valueSet will already be set true because Put() will skip the
+	 * while loop and put the value and set valueSet=true. Then when consumer reach, valueSet=true so it will not go inside
+	 * while loop and print the get() value instead
+	 */
+	public synchronized void get() {		
+		while(!valueSet) {					//when valueSet=false, new value is not set, so this has to wait
 			try{wait();} catch(Exception e) {}
 		}
 		System.out.println("Get: "+value);
 		valueSet=false;
-		notify();
+		notify();     		//notifies the set() that it has already been set,and that it can proceed now
 	}
 }
 
